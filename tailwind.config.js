d11153294bc06392f4602a14c625f240c280ee99
pabloarshid <umarsa.birvancity@gmail.com>
2024-03-19 21:49:41 -0700

    NBA Dashboard app

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#3490dc', // Your primary color
+        'secondary': '#ffed4a', // Your secondary color
+        // Add other colors as needed
+    },
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'], // Example custom font
+      // Define other font families as needed
+      },
+      plugins: [],
+    }
+  }
+}
